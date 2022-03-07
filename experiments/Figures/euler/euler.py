3c5727da89d064132be99dd9a44e4796d05e6843
EA264728 <elouan.argouarch@cea.fr>
2022-03-07 10:58:31 +0100

    simus

--- experiments/Figures/euler/euler.py
+++ experiments/Figures/euler/euler.py
@@ -2,7 +2,6 @@ from matplotlib import image
 import numpy as np
 import torch
 from torch import nn
-import pickle
 
 import sys
 from pathlib import Path
@@ -27,14 +26,14 @@ grey = torch.tensor(rgb2gray(rgb))
 vector_density = grey.flatten()
 vector_density = vector_density/torch.sum(vector_density)
 lignes, colonnes = grey.shape
-num_samples = 500000
+num_samples = 5000
 cat = torch.distributions.Categorical(probs = vector_density)
 categorical_samples = cat.sample([num_samples])
 target_samples = torch.cat([(categorical_samples//colonnes).unsqueeze(-1), (categorical_samples%colonnes).unsqueeze(-1)], dim = -1) + torch.rand([num_samples,2])
 
 #Save target sampels
 filename = './experiments/Figures/euler/euler_samples.sav'
-pickle.dump(target_samples,open(filename,'wb'))
+torch.save(target_samples,filename)
 
 #Run EM
 linspace_x = 8
@@ -43,15 +42,15 @@ K = linspace_x*linspace_y
 initial_m = torch.cartesian_prod(torch.linspace(0, lignes,linspace_x),torch.linspace(0, colonnes, linspace_y))
 EM = EMDensityEstimator(target_samples,K)
 EM.mu = initial_m
-epochs = 5
+epochs = 1
 EM.train(epochs)
 
 #Save em
 filename = './experiments/Figures/euler/euler_em.sav'
-pickle.dump(EM,open(filename,'wb'))
+torch.save(EM,filename)
 
 #Run DIF with initialization EM
-epochs = 100
+epochs = 1
 batch_size = 20000
 initial_T = LocationScaleFlow(K,2)
 initial_T.m = nn.Parameter(EM.m)
@@ -68,4 +67,4 @@ dif.train(epochs, batch_size)
 
 #Save dif
 filename = './experiments/Figures/euler/euler_dif.sav'
-pickle.dump(dif,open(filename,'wb'))
+torch.save(dif,filename)
