0030937d562a6191781640f6adddefd62709a41d
EA264728 <elouan.argouarch@cea.fr>
2022-03-10 11:38:12 +0100

    setup wip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Discretely-Indexed-Flows',
+    version='',
+    packages=['models'],
+    url='',
+    license='',
+    author='EA264728',
+    author_email='elouan.argouarch@gmail.com',
+    description=''
+)
