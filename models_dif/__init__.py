330089e331ddbc06ef1839510ab81c373afa7813
EA264728 <elouan.argouarch@cea.fr>
2022-08-25 14:58:24 +0200

    fix

--- models_dif/__init__.py
+++ models_dif/__init__.py
@@ -1,6 +1,6 @@
 from .dif_density_estimator import DIFDensityEstimator
 from .dif_conditional_density_estimator import ConditionalDIFDensityEstimator
 from .dif_sampler import DIFSampler
-from .location_scale_flow import LocationScaleFlow, FullRankLocationScaleFlow
+from .location_scale_flow import LocationScaleFlow
 from .softmax_weight import SoftmaxWeight
 from .tmc import TMC
