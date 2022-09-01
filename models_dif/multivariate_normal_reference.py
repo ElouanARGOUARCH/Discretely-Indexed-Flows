52372a2adcbea43c5e7ed206fc43bd5e0692db4b
EA264728 <elouan.argouarch@cea.fr>
2022-09-01 19:59:04 +0200

    explicitely made cov matrix symetrical

--- models_dif/multivariate_normal_reference.py
+++ models_dif/multivariate_normal_reference.py
@@ -20,6 +20,6 @@ class MultivariateNormalReference(nn.Module):
         #return torch.randn([num_samples, self.p])
 
     def log_density(self, z):
-        distribution = torch.distributions.MultivariateNormal(self.mean.to(z.device), self.cov.to(z.device))
+        distribution = torch.distributions.MultivariateNormal(self.mean.to(z.device), self.cov.to(z.device)).to(z.device)
         return self.distribution.log_prob(z)
         #return -torch.sum(torch.square(z)/2, dim = -1) - torch.log(torch.tensor([2*torch.pi], device = z.device))*self.p/2
